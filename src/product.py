7a5b1e6bdaec1111f5d798c76235db38a2e4de2b
Saidova <saidova.kseniya@mail.ru>
2025-06-26 20:51:04 +0300

    Для класса Product: - Цена стала приватным атрибутом (_price) - Добавлены геттер и сеттер для цены с проверкой - Реализован класс-метод new_product() для создания продукта из словаря

--- src/product.py
+++ src/product.py
@@ -10,12 +10,41 @@ class Product:
         :param price: Цена товара (с копейками).
         :param quantity: Количество товара в наличии.
         """
+        self.name = name
+        self.description = description
+        self._price = price  # Приватный атрибут
+        self.quantity = quantity
+
         if price <= 0:
             raise ValueError("Цена должна быть положительной.")
         if quantity < 0:
             raise ValueError("Количество не может быть отрицательным.")
 
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
+    @classmethod
+    def new_product(cls, product_data: dict):
+        """
+        Класс-метод для создания нового продукта из словаря.
+
+        :param product_data: Словарь с данными продукта.
+        :return: Объект класса Product.
+        """
+        return cls(
+            name=product_data['name'],
+            description=product_data['description'],
+            price=product_data['price'],
+            quantity=product_data['quantity']
+        )
+
+    @property
+    def price(self):
+        """Геттер для цены."""
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        """Сеттер для цены с проверкой."""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self._price = new_price
+
