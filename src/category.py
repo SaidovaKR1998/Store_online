a2b011fb35e775b0138533842a4adb0f0c6d0398
Saidova <saidova.kseniya@mail.ru>
2025-06-26 20:51:42 +0300

    Для класса Category: - Список товаров стал приватным (__products) - Добавлен метод add_product() для добавления товаров - Реализовано свойство products через @property для вывода информации о товарах

--- src/category.py
+++ src/category.py
@@ -1,3 +1,8 @@
+from itertools import product
+
+from src.product import Product
+
+
 class Category:
     """Класс для представления категории товаров."""
 
@@ -14,8 +19,25 @@ class Category:
         """
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products  # Приватный атрибут
 
         # Увеличиваем счетчики при создании категории
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product: Product):
+        """
+        Метод для добавления товара в категорию.
+
+        :param product: Объект класса Product.
+        """
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        """Геттер для списка товаров."""
+        return "\n".join(
+            [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+             for product in self.__products]
+        )
